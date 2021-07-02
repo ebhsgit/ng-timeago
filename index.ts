806e96f296b43d8a506d05efb6472ee2e56285ff
8bhsolutions <48874658+8bhsolutions@users.noreply.github.com>
2021-07-02 14:33:45 +1000

    added index.ts

---
+++ index.ts
@@ -0,0 +1,6 @@
+export * from './src/timeago.directive';
+export * from './src/timeago.pipe';
+export * from './src/timeago.intl';
+export * from './src/timeago.clock';
+export * from './src/timeago.formatter';
+export * from './src/timeago.module';
