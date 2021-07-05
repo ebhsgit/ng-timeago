8fb8c2ef4ed7cc6cef2ecab8da3869a853ab5b6d
8bhsolutions <48874658+8bhsolutions@users.noreply.github.com>
2021-07-05 15:28:57 +1000

    set ts target to 2015

--- dist/timeago.clock.js
+++ dist/timeago.clock.js
@@ -1,38 +1,21 @@
 "use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 Object.defineProperty(exports, "__esModule", { value: true });
-var core_1 = require("@angular/core");
-var rxjs_1 = require("rxjs");
-var of_1 = require("rxjs/observable/of");
-var timer_1 = require("rxjs/observable/timer");
-var operators_1 = require("rxjs/operators");
-var util_1 = require("./util");
-var TimeagoClock = /** @class */ (function () {
-    function TimeagoClock() {
-    }
-    return TimeagoClock;
-}());
+const core_1 = require("@angular/core");
+const rxjs_1 = require("rxjs");
+const of_1 = require("rxjs/observable/of");
+const timer_1 = require("rxjs/observable/timer");
+const operators_1 = require("rxjs/operators");
+const util_1 = require("./util");
+class TimeagoClock {
+}
 exports.TimeagoClock = TimeagoClock;
-var TimeagoDefaultClock = /** @class */ (function (_super) {
-    __extends(TimeagoDefaultClock, _super);
-    function TimeagoDefaultClock() {
-        return _super !== null && _super.apply(this, arguments) || this;
-    }
-    TimeagoDefaultClock.prototype.tick = function (then) {
+class TimeagoDefaultClock extends TimeagoClock {
+    tick(then) {
         return of_1.of(0)
-            .pipe(operators_1.expand(function () {
-            var now = Date.now();
-            var seconds = Math.round(Math.abs(now - then) / 1000);
-            var period = seconds < util_1.MINUTE
+            .pipe(operators_1.expand(() => {
+            const now = Date.now();
+            const seconds = Math.round(Math.abs(now - then) / 1000);
+            const period = seconds < util_1.MINUTE
                 ? 1000
                 : seconds < util_1.HOUR
                     ? 1000 * util_1.MINUTE
@@ -41,11 +24,10 @@ var TimeagoDefaultClock = /** @class */ (function (_super) {
                         : 0;
             return period ? timer_1.timer(period) : rxjs_1.Observable.empty();
         }), operators_1.skip(1));
-    };
-    TimeagoDefaultClock.decorators = [
-        { type: core_1.Injectable },
-    ];
-    return TimeagoDefaultClock;
-}(TimeagoClock));
+    }
+}
+TimeagoDefaultClock.decorators = [
+    { type: core_1.Injectable },
+];
 exports.TimeagoDefaultClock = TimeagoDefaultClock;
 //# sourceMappingURL=timeago.clock.js.map
--- dist/timeago.intl.js
+++ dist/timeago.intl.js
@@ -1,24 +1,23 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var core_1 = require("@angular/core");
-var rxjs_1 = require("rxjs");
+const core_1 = require("@angular/core");
+const rxjs_1 = require("rxjs");
 ;
 /**
  * To modify the text displayed, create a new instance of TimeagoIntl and
  * include it in a custom provider
  */
-var TimeagoIntl = /** @class */ (function () {
-    function TimeagoIntl() {
+class TimeagoIntl {
+    constructor() {
         /**
            * Stream that emits whenever the l10n strings are changed
            * Use this to notify directives if the l10n strings have changed after initialization.
            */
         this.changes = new rxjs_1.Subject();
     }
-    TimeagoIntl.decorators = [
-        { type: core_1.Injectable },
-    ];
-    return TimeagoIntl;
-}());
+}
+TimeagoIntl.decorators = [
+    { type: core_1.Injectable },
+];
 exports.TimeagoIntl = TimeagoIntl;
 //# sourceMappingURL=timeago.intl.js.map
